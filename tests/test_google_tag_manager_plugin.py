c564738c4b439557ad811d396d0dbc3b9397c838
Krzysztof <krzysztof.kolodzinski@problematy.pl>
2025-02-19 00:15:51 +0100

    fixed tests

--- tests/test_google_tag_manager_plugin.py
+++ tests/test_google_tag_manager_plugin.py
@@ -14,7 +14,7 @@ def test_renders_head_code_with_gtm_id():
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-XXXX');</script>
-        <!-- End Google Tag Manager -->"""
+        <!-- End Google Tag Manager -->\n    """
     )
     assert result == app
 
@@ -22,11 +22,15 @@ def test_renders_body_code_with_gtm_id():
     app = Mock()
     plugin_config = {"ID": "GTM-XXXX"}
 
+    result = process(app, plugin_config)
+
     app.add_dynamic_body.assert_called_once_with(
         """<!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXX"
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXX
+        "
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->"""
+        <!-- End Google Tag Manager (noscript) -->
+    """
     )
     assert result == app
 
@@ -43,12 +47,15 @@ def test_handles_empty_gtm_id():
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','');</script>
-        <!-- End Google Tag Manager -->"""
+        <!-- End Google Tag Manager -->
+    """
     )
     app.add_dynamic_body.assert_called_once_with(
         """<!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id="
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=
+        "
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->"""
+        <!-- End Google Tag Manager (noscript) -->
+    """
     )
     assert result == app
